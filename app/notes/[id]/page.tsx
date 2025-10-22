608af21cac50cf3492d68b041fcc29dc21a67987
Katerynakulik <katerynakulik4@gmail.com>
2025-10-22 10:46:42 +0300

    Change Note interface and page.tsx in id

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,25 +1,27 @@
-import { getSingleNote } from "@/lib/api";
+import { fetchNoteById } from "@/lib/api";
+import NoteDetailsClient from "./NoteDetails.client";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 
-type Props = {
+interface NoteDetailsProps {
   params: Promise<{ id: string }>;
-};
+}
 
-const NoteDetails = async ({ params }: Props) => {
+export default async function NoteDetails({ params }: NoteDetailsProps) {
   const { id } = await params;
-  const note = await getSingleNote(id);
+  const queryClient = new QueryClient();
 
-  const formattedDate = note.updatedAt
-    ? `Updated at: ${note.updatedAt}`
-    : `Created at: ${note.createdAt}`;
+  await queryClient.prefetchQuery({
+    queryKey: ["note", id],
+    queryFn: () => fetchNoteById(id),
+  });
 
   return (
-    <div>
-      <h2>{note.title}</h2>
-      <p>{note.content}</p>
-      <button>Edit</button>
-      <p>{formattedDate}</p>
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NoteDetailsClient />
+    </HydrationBoundary>
   );
-};
-
-export default NoteDetails;
+}
--- lib/api.ts
+++ lib/api.ts
@@ -42,11 +42,6 @@ export async function deleteNote(id: string): Promise<Note> {
   return response.data;
 }
 
-export const getSingleNote = async (id: string) => {
-  const res = await api.get<Note>(`/notes/${id}`);
-  return res.data;
-};
-
 export async function fetchNoteById(id: string): Promise<Note> {
   const response = await api.get<Note>(`/notes/${id}`);
   return response.data;
